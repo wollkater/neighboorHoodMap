3f2a3a2088750689465ec23e48be3d117001171b
Marcel Raddatz <raddatz@gates-services.de>
2018-03-09 14:13:20 +0100

    speed up the loading process

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-function Place(name, latLng, rating, reviews) {
+const MUENSTER = {lat: 51.963066, lng: 7.626245};
+
+function Place(id, name, latLng, rating, reviews, marker) {
     const self = this;
+    self.id = id;
     self.name = name;
     self.latLng = latLng;
     self.rating = ko.observable(rating);
@@ -7,6 +10,7 @@ function Place(name, latLng, rating, reviews) {
     self.filledStars = ko.observableArray(Array(Math.round(rating)));
     self.emptyStars = Array(5 - Math.round(rating));
     self.reviews = ko.observableArray(reviews);
+    self.marker = marker;
 }
 
 
@@ -14,93 +18,74 @@ function Place(name, latLng, rating, reviews) {
 function MapViewModel() {
     const self = this;
     let placeArray = [];
-    let map;
-    let markers = []; //Array that holds markers so we can clear them later
     self.loading = ko.observable(true);
     self.filterString = ko.observable("");
     self.currentLocation = ko.observable(null);
     self.locations = ko.observableArray(placeArray);
     self.showDetails = ko.observable(false);
     self.showList = ko.observable(true);
-
-    //get initial data for Münster in Germany
-    function initMap() {
-        placeArray = [];
-        const muenster = {lat: 51.963066, lng: 7.626245};
-         map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 13,
-            center: muenster
-        });
-
-         $.ajax({
-             url: `/api/yelp/businesses/search?term=food&latitude=${muenster.lat}&longitude=${muenster.lng}`,
-             success: onBusinessesReceived,
-             error: () => onError("Couldn't fetch businesses")
-                });
-    }
+    self.map;
 
     //Success handler for businesses
-    function onBusinessesReceived(data) {
+    self.onBusinessesReceived = (data) => {
         if(data.businesses) {
-            //load reviews for all businesses
-            data.businesses.forEach(business => {
-				$.ajax({
-						   url: `/api/yelp/businesses/${business.id}/reviews`,
-						   async: false, //if we make it async, we exceed the request per second limit
-						   success: (reviews) => onReviewsReceived(business, reviews),
-						   error: () => onError(`Couldn't fetch reviews for ${business.name}`)
-					   });
+            data.businesses.forEach((business) => {
+				//convert Yelp coordinates to Google coordinates
+				const latLng = {lat:business.coordinates.latitude, lng:business.coordinates.longitude};
+				placeArray.push(new Place(business.id, business.name, latLng, business.rating));
             });
-			self.loading(false);
+			initMarker(placeArray);
         }
-    }
+		self.loading(false);
+	};
 
-    //Success handler for reviews
-    function onReviewsReceived(business, reviews) {
-        //convert Yelp coordinates to Google coordinates
-        const latLng = {lat:business.coordinates.latitude, lng:business.coordinates.longitude};
-        placeArray.push(new Place(business.name, latLng, business.rating, reviews.reviews));
 
-        initMarker(map, placeArray);
-    }
 
-    function onError(message) {
-        alert(message);
-    }
-
-    function initMarker(map, places) {
-        //Clear markers
-        markers.forEach(marker => {
-            marker.setMap(null);
-        });
-
+    function initMarker(places) {
         self.locations(places);
         self.currentLocation(places[0]);
         //Place new marker
-        places.forEach(place => {
-            const marker = new google.maps.Marker({
-                                                      position: place.latLng,
-                                                      map: map,
-                                                      title: place.name,
-                                                      animation: google.maps.Animation.DROP,
-                                                  });
-            marker.addListener("click", event => {
-                self.currentLocation(place);
-                self.showDetails(true);
-                marker.setAnimation(google.maps.Animation.BOUNCE);
-                setTimeout(() => marker.setAnimation(null), 1000);
-            });
-            markers.push(marker);
-        });
+		places.forEach(place => createMarker(place, self.map));
     }
 
-    initMap();
+    function createMarker(place, map) {
+		const marker = new google.maps.Marker({
+												  position: place.latLng,
+												  map: map,
+												  title: place.name,
+												  animation: google.maps.Animation.DROP,
+											  });
+		marker.addListener("click", (event) => self.openReviews(place));
+		place.marker = marker;
+	}
+
+    self.openReviews = function(place) {
+        self.loading(true);
+		$.ajax({
+				   url: `/api/yelp/businesses/${place.id}/reviews`,
+				   success: (data) => {
+				       place.reviews = data.reviews;
+					   self.currentLocation(place);
+					   const marker = place.marker;
+					   marker.setAnimation(google.maps.Animation.BOUNCE);
+					   setTimeout(() => marker.setAnimation(null), 1000);
+					   self.showDetails(true);
+					   self.loading(false);
+				   },
+				   error: () => onError(`Couldn't fetch reviews for ${place.name}`)
+			   });
+	};
 
     //Simple name filter
     self.filterPlaces = (input) => {
-        const places = placeArray.filter(value => {return value.name.toLowerCase().includes(input.toLowerCase());});
-        self.locations(places);
-        initMarker(map, places);
+        const placesSearchedFor = placeArray
+		.filter(value => {return value.name.toLowerCase().includes(input.toLowerCase());});
+
+        //Remove markers from the map
+        placeArray.forEach(place => place.marker.setMap(null));
+
+        self.locations(placesSearchedFor);
+        initMarker(placesSearchedFor);
     };
 
 
@@ -108,4 +93,21 @@ function MapViewModel() {
 const vm = new MapViewModel();
 ko.applyBindings(vm);
 vm.filterString.subscribe((val)=>vm.filterPlaces(val));
+//get initial data for Münster in Germany
+function initMap() {
+    vm.loading(true);
+	vm.map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 13,
+		center: MUENSTER
+	});
 
+	$.ajax({
+			   url: `/api/yelp/businesses/search?term=food&latitude=${MUENSTER.lat}&longitude=${MUENSTER.lng}`,
+			   success: (data) => vm.onBusinessesReceived(data, map),
+			   error: () => onError("Couldn't fetch businesses")
+		   });
+}
+function onError(message) {
+	vm.loading(false);
+	alert(message);
+}
